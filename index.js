d495276b5586c838dc6f44508276bb73a2ac00c4
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2022-05-16 02:14:39 +0600

    Limit Dashboard features based on admin level access

--- index.js
+++ index.js
@@ -46,6 +46,35 @@ async function run() {
             res.send(service)
         })
 
+        app.get('/user', jwtToken, async (req, res) => {
+            const users = await userCollection.find().toArray()
+            res.send(users)
+        })
+
+        app.get('/user/:email', async (req, res) => {
+            const email = req.params.email
+            const user = await userCollection.findOne({ email: email })
+            const isAdmin = user.role === 'admin'
+            res.send({ admin: isAdmin })
+        })
+
+        app.put('/user/admin/:email', jwtToken, async (req, res) => {
+            const email = req.params.email
+            const requester = req.decoded.email
+            const requesterAccount = await userCollection.findOne({ email: requester })
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email }
+                const updateDoc = {
+                    $set: { role: 'admin' },
+                }
+                const result = await userCollection.updateOne(filter, updateDoc)
+                res.send(result)
+
+            } else {
+                res.status(403).send({ message: 'Forbidden' })
+            }
+        })
+
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email
             const user = req.body
@@ -87,8 +116,8 @@ async function run() {
                 const query = { patient: patient }
                 const booking = await bookingCollection.find(query).toArray()
                 return res.send(booking)
-            }else{
-                return res.status(403).send({ message: 'Forbidden access' }) 
+            } else {
+                return res.status(403).send({ message: 'Forbidden access' })
             }
             //const authorization = req.headers.authorization
             //console.log('auth headers', authorization)
